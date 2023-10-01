9b9cf97c167947723a837685260f9c57c965a290
Luisfgt <luisfgonzalezt09@gmail.com>
2023-09-30 20:38:26 -0500

    1ra parte

---
+++ front/src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+
+
+const SearchBar = ({onSearch}) => {
+   return (
+      <div>
+         <input type='search' />
+         <button onClick={onSearch}>Agregar</button>
+      </div>
+   );
+}
+
+export default SearchBar
